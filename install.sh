84ee031abbaff34b0e36b527ec00ffeab6f894b1
Fedor Scholz <scholz@fzi.de>
2018-02-12 12:39:34 +0100

    check if repo exists before cloning

--- install.sh
+++ install.sh
@@ -8,8 +8,21 @@ cd `dirname $0`
 pip install -r requirements.txt
 
 cd $HBP/GazeboRosPackages/src
-git clone git@github.com:HBPNeurorobotics/embodied_attention.git
-git clone git@github.com:HBPNeurorobotics/holographic.git
+
+if cd embodied_attention; then
+  git pull
+  cd ..
+else
+  git clone git@github.com:HBPNeurorobotics/embodied_attention.git
+fi
+
+if cd holographic; then
+  git pull
+  cd ..
+else
+  git clone git@github.com:HBPNeurorobotics/holographic.git
+fi
+
 cd ..
 catkin_make
 
