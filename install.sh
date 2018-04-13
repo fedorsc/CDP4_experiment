b4e50a0c37e421b2c3957df1d8bcb6a3f5d67a3e
Fedor Scholz <scholz@fzi.de>
2018-04-13 17:59:25 +0200

    download one model only

--- install.sh
+++ install.sh
@@ -54,17 +54,9 @@ cd ../..
 # install pretrained models
 mkdir -p $HOME/.opt/graph_def
 cd /tmp
-for model in \
-  ssd_mobilenet_v1_coco_11_06_2017 \
-  ssd_inception_v2_coco_11_06_2017 \
-  rfcn_resnet101_coco_11_06_2017 \
-  faster_rcnn_resnet101_coco_11_06_2017 \
-  faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017
-do \
-  curl -OL http://download.tensorflow.org/models/object_detection/$model.tar.gz
-  tar -xzf $model.tar.gz $model/frozen_inference_graph.pb
-  cp -a $model $HOME/.opt/graph_def/
-done
+curl -OL http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
+tar -xzf faster_rcnn_resnet101_coco_11_06_2017.tar.gz faster_rcnn_resnet101_coco_11_06_2017/frozen_inference_graph.pb
+cp -a faster_rcnn_resnet101_coco_11_06_2017 $HOME/.opt/graph_def/
 ln -sf $HOME/.opt/graph_def/faster_rcnn_resnet101_coco_11_06_2017/frozen_inference_graph.pb $HOME/.opt/graph_def/frozen_inference_graph.pb
 
 cd $DIR
