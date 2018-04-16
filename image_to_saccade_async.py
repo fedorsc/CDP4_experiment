22d43b58a42a141c291bd6d51dbc501d0d7ef3be
Fedor Scholz <scholz@fzi.de>
2018-04-16 17:05:34 +0200

    add asynchronous versions of transfer function

---
+++ image_to_saccade_async.py
@@ -0,0 +1,75 @@
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from cv_bridge import CvBridge
+from attention import Saliency
+from std_msgs.msg import Float32MultiArray
+
+tensorflow_path = rospy.get_param("~tensorflow_path", "/opt/tensorflow_venv/lib/python2.7/site-packages")
+model_file = rospy.get_param('~saliency_file', '/tmp/model.ckpt')
+network_input_height = float(rospy.get_param('~network_input_height', '192'))
+network_input_width = float(rospy.get_param('~network_input_width', '256'))
+
+@nrp.MapVariable("saliency", initial_value = Saliency(tensorflow_path, model_file, network_input_height, network_input_width))
+@nrp.MapVariable("saliency_pub", initial_value = rospy.Publisher("/saliency_map", Float32MultiArray, queue_size=1))
+@nrp.MapVariable("saliency_image_pub", initial_value = rospy.Publisher("/saliency_map_image", Image, queue_size=1))
+@nrp.MapVariable("bridge", initial_value=CvBridge())
+@nrp.MapRobotSubscriber("image", Topic("/hollie/camera/left/image_raw", Image))
+def image_to_saliency(t, saliency, saliency_pub, saliency_image_pub, bridge, image):
+    if image.value is None:
+        return
+
+    image = bridge.value.imgmsg_to_cv2(image.value, "bgr8")
+    saliency_map = saliency.value.compute_saliency_map(image)
+    saliency_map_image = bridge.value.cv2_to_imgmsg(np.uint8(saliency_map * 255.), "mono8")
+    saliency_image_pub.value.publish(saliency_map_image)
+
+    from std_msgs.msg import Float32MultiArray, MultiArrayDimension, MultiArrayLayout
+    height = MultiArrayDimension(size=len(saliency_map))
+    width = MultiArrayDimension(size=len(saliency_map[0]))
+    lo = MultiArrayLayout([height, width], 0)
+    saliency_pub.value.publish(Float32MultiArray(layout=lo, data=saliency_map.flatten()))
+
+import rospy
+from attention import Saccade
+from rosgraph_msgs.msg import Clock
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Image
+
+@nrp.MapVariable("saccade", initial_value = Saccade(), scope=nrp.GLOBAL)
+@nrp.MapVariable("target_pub", initial_value = rospy.Publisher("/saccade_target", Point, queue_size=1))
+@nrp.MapVariable("potential_target_pub", initial_value = rospy.Publisher("/saccade_potential_target", Point, queue_size=1))
+@nrp.MapVariable("bridge", initial_value=CvBridge())
+@nrp.MapVariable("visual_neurons_pub", initial_value = rospy.Publisher("/visual_neurons", Image, queue_size=1))
+@nrp.MapVariable("motor_neurons_pub", initial_value = rospy.Publisher("/motor_neurons", Image, queue_size=1))
+@nrp.MapRobotSubscriber("saliency_map", Topic("/saliency_map", Float32MultiArray))
+@nrp.MapRobotSubscriber("clock", Topic("/clock", Clock))
+@nrp.Neuron2Robot(triggers = "clock", throttling_rate = 1000)
+def saliency_to_saccade(t, saccade, target_pub, potential_target_pub, saliency_map, bridge, visual_neurons_pub, motor_neurons_pub, clock):
+    if saliency_map.value is None:
+        return
+
+    lo = saliency_map.value.layout
+    saliency_map_extracted = np.asarray(saliency_map.value.data[lo.data_offset:]).reshape(lo.dim[0].size, lo.dim[1].size)
+    (target, is_actual_target, V, M) = saccade.value.compute_saccade_target(saliency_map_extracted, 1)
+    target = Point(target[0], target[1], target[2])
+    potential_target_pub.value.publish(target)
+    if is_actual_target:
+        target_pub.value.publish(target)
+
+    V = (V - V.min()) / (V.max() - V.min())                                                                                                                                                                          
+    M = (M - M.min()) / (M.max() - M.min())
+    
+    visual_neurons_image = bridge.value.cv2_to_imgmsg(np.uint8(V * 255.), "mono8")
+    motor_neurons_image = bridge.value.cv2_to_imgmsg(np.uint8(M * 255.), "mono8")
+    visual_neurons_pub.value.publish(visual_neurons_image)
+    motor_neurons_pub.value.publish(motor_neurons_image)
+
+from attention import Saccade
+from std_msgs.msg import Empty
+
+@nrp.MapVariable("saccade", initial_value=Saccade(), scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("shift", Topic("/shift", Empty))
+@nrp.Neuron2Robot(triggers = "shift")
+def shift_callback(t, saccade, shift):
+    saccade.value.shift()
---
+++ image_to_saccade_curiosity_async.py
@@ -0,0 +1,136 @@
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from cv_bridge import CvBridge
+from attention import Saliency
+from std_msgs.msg import Float32MultiArray
+from image_geometry import StereoCameraModel
+
+tensorflow_path = rospy.get_param("~tensorflow_path", "/opt/tensorflow_venv/lib/python2.7/site-packages")
+model_file = rospy.get_param('~saliency_file', '/tmp/model.ckpt')
+network_input_height = float(rospy.get_param('~network_input_height', '192'))
+network_input_width = float(rospy.get_param('~network_input_width', '256'))
+
+@nrp.MapVariable("saliency", initial_value = Saliency(tensorflow_path, model_file, network_input_height, network_input_width))
+@nrp.MapVariable("saliency_pub", initial_value = rospy.Publisher("/saliency_map", Float32MultiArray, queue_size=1))
+@nrp.MapVariable("saliency_image_pub", initial_value = rospy.Publisher("/saliency_map_image", Image, queue_size=1))
+@nrp.MapVariable("bridge", initial_value=CvBridge())
+@nrp.MapVariable("points", initial_value=[], scope=nrp.GLOBAL)
+@nrp.MapVariable("camera_model", initial_value=StereoCameraModel())
+@nrp.MapVariable("camera_info_left", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("camera_info_right", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("disparity_image", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("tfBuffer", initial_value=None)
+@nrp.MapVariable("listener", initial_value=None)
+@nrp.MapRobotSubscriber("image", Topic("/hollie/camera/left/image_raw", Image))
+def image_to_saliency(t, image, bridge, saliency, saliency_pub, saliency_image_pub, points, camera_model, camera_info_left, camera_info_right, disparity_image, tfBuffer, listener):
+    if image.value is None or camera_info_left.value is None or camera_info_right.value is None or disparity_image.value is None:
+        return
+
+    if tfBuffer.value is None or listener.value is None:
+        import rospy
+        import tf2_ros
+        tfBuffer.value = tf2_ros.Buffer(rospy.Duration(30))
+        listener.value = tf2_ros.TransformListener(tfBuffer.value)
+
+    image = bridge.value.imgmsg_to_cv2(image.value, "bgr8")
+    saliency_map = saliency.value.compute_saliency_map(image)
+
+    # apply curiosity
+    import rospy
+    camera_model.value.fromCameraInfo(camera_info_left.value, camera_info_right.value)
+    disparity_image = bridge.value.imgmsg_to_cv2(disparity_image.value.image)
+    for point in points.value:
+        # point.header.stamp = rospy.Time.now()
+        transformed = tfBuffer.value.transform(point, camera_model.value.tfFrame(), timeout=rospy.Duration(0.1))
+        point_torso = (-transformed.point.y, -transformed.point.z, transformed.point.x)
+        pixel = camera_model.value.project3dToPixel(point_torso)
+        x = int(pixel[0][0] * (len(saliency_map[0])/float(camera_info_left.value.width)))
+        y = int(pixel[0][1] * (len(saliency_map)/float(camera_info_left.value.height)))
+        disparity = camera_model.value.getDisparity(point_torso[2])
+        x = x + disparity
+        if x >= 0 and x < len(saliency_map[0]) and y >=0 and y < len(saliency_map):
+            from skimage.draw import circle
+            rr, cc = circle(y, x, 25, (len(saliency_map), len(saliency_map[0])))
+            saliency_map[rr, cc] = 0.
+
+    saliency_map_image = bridge.value.cv2_to_imgmsg(np.uint8(saliency_map * 255.), "mono8")
+    saliency_image_pub.value.publish(saliency_map_image)
+
+    from std_msgs.msg import Float32MultiArray, MultiArrayDimension, MultiArrayLayout
+    height = MultiArrayDimension(size=len(saliency_map))
+    width = MultiArrayDimension(size=len(saliency_map[0]))
+    lo = MultiArrayLayout([height, width], 0)
+    saliency_pub.value.publish(Float32MultiArray(layout=lo, data=saliency_map.flatten()))
+
+import rospy
+from attention import Saccade
+from rosgraph_msgs.msg import Clock
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Image
+
+@nrp.MapVariable("saccade", initial_value = Saccade(), scope=nrp.GLOBAL)
+@nrp.MapVariable("target_pub", initial_value = rospy.Publisher("/saccade_target", Point, queue_size=1))
+@nrp.MapVariable("potential_target_pub", initial_value = rospy.Publisher("/saccade_potential_target", Point, queue_size=1))
+@nrp.MapVariable("bridge", initial_value=CvBridge())
+@nrp.MapVariable("visual_neurons_pub", initial_value = rospy.Publisher("/visual_neurons", Image, queue_size=1))
+@nrp.MapVariable("motor_neurons_pub", initial_value = rospy.Publisher("/motor_neurons", Image, queue_size=1))
+@nrp.MapRobotSubscriber("saliency_map", Topic("/saliency_map", Float32MultiArray))
+@nrp.MapRobotSubscriber("clock", Topic("/clock", Clock))
+@nrp.Neuron2Robot(triggers = "clock", throttling_rate = 1000)
+def saliency_to_saccade(t, saccade, target_pub, potential_target_pub, saliency_map, bridge, visual_neurons_pub, motor_neurons_pub, clock):
+    if saliency_map.value is None:
+        return
+
+    lo = saliency_map.value.layout
+    saliency_map_extracted = np.asarray(saliency_map.value.data[lo.data_offset:]).reshape(lo.dim[0].size, lo.dim[1].size)
+    (target, is_actual_target, V, M) = saccade.value.compute_saccade_target(saliency_map_extracted, 1)
+    target = Point(target[0], target[1], target[2])
+    potential_target_pub.value.publish(target)
+    if is_actual_target:
+        target_pub.value.publish(target)
+
+    V = (V - V.min()) / (V.max() - V.min())                                                                                                                                                                          
+    M = (M - M.min()) / (M.max() - M.min())
+    
+    visual_neurons_image = bridge.value.cv2_to_imgmsg(np.uint8(V * 255.), "mono8")
+    motor_neurons_image = bridge.value.cv2_to_imgmsg(np.uint8(M * 255.), "mono8")
+    visual_neurons_pub.value.publish(visual_neurons_image)
+    motor_neurons_pub.value.publish(motor_neurons_image)
+
+from tf2_geometry_msgs import PointStamped 
+@nrp.MapVariable("points", initial_value=[], scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("point", Topic("/saccade_point", PointStamped))
+@nrp.Robot2Neuron(triggers="point")
+def point_callback(t, point, points):
+    points.value.append(point.value)
+
+from sensor_msgs.msg import CameraInfo
+@nrp.MapVariable("camera_info_left", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("camera_info", Topic("/hollie/camera/left/camera_info", CameraInfo))
+@nrp.Robot2Neuron(triggers="camera_info")
+def camera_info_left_callback(t, camera_info, camera_info_left):
+    camera_info_left.value = camera_info.value
+
+from sensor_msgs.msg import CameraInfo
+@nrp.MapVariable("camera_info_right", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("camera_info", Topic("/hollie/camera/right/camera_info", CameraInfo))
+@nrp.Robot2Neuron(triggers="camera_info")
+def camera_info_right_callback(t, camera_info, camera_info_right):
+    camera_info_right.value = camera_info.value
+
+from stereo_msgs.msg import DisparityImage
+@nrp.MapVariable("disparity_image", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("disparity_image_new", Topic("/hollie/camera/disparity", DisparityImage))
+@nrp.Robot2Neuron(triggers="disparity_image_new")
+def disparity_image_callback(t, disparity_image_new, disparity_image):
+    disparity_image.value = disparity_image_new.value
+
+from attention import Saccade
+from std_msgs.msg import Empty
+
+@nrp.MapVariable("saccade", initial_value=Saccade(), scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("shift", Topic("/shift", Empty))
+@nrp.Neuron2Robot(triggers = "shift")
+def shift_callback(t, saccade, shift):
+    saccade.value.shift()
