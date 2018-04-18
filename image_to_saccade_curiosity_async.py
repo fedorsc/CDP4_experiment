daa0a1bdf64f811e87b485bd79de0ac3345c0704
Fedor Scholz <scholz@fzi.de>
2018-04-18 16:26:40 +0200

    add listener for saccade mechanism reset

--- image_to_saccade_curiosity_async.py
+++ image_to_saccade_curiosity_async.py
@@ -137,3 +137,13 @@ from std_msgs.msg import Empty
 @nrp.Neuron2Robot(triggers = "shift")
 def shift_callback(t, saccade, shift):
     saccade.value.shift()
+
+from attention import Saccade
+from std_msgs.msg import Empty
+
+@nrp.MapVariable("saccade", initial_value=Saccade(), scope=nrp.GLOBAL)
+@nrp.MapRobotSubscriber("reset", Topic("/reset", Empty))
+@nrp.Neuron2Robot(triggers = "reset")
+def reset_callback(t, saccade, reset):
+    from attention import Saccade
+    saccade.value = Saccade()
