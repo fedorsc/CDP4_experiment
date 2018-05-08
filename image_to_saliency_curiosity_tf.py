915dd2a555f09871cac7aa74265cad7bf51b03a2
Fedor Scholz <scholz@fzi.de>
2018-05-08 18:03:16 +0200

    move to no tf by default

--- image_to_saliency_curiosity_tf.py
+++ image_to_saliency_curiosity_tf.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Point
 from cv_bridge import CvBridge
 from attention import Saliency
 from std_msgs.msg import Float32MultiArray
-from image_geometry import PinholeCameraModel
+from image_geometry import StereoCameraModel
 from embodied_attention.srv import Transform
 
 tensorflow_path = rospy.get_param("tensorflow_path", "/opt/tensorflow_venv/lib/python2.7/site-packages")
@@ -17,18 +17,19 @@ network_input_width = float(rospy.get_param('~network_input_width', '256'))
 @nrp.MapVariable("saliency_image_pub", initial_value = rospy.Publisher("/saliency_map_image", Image, queue_size=1))
 @nrp.MapVariable("bridge", initial_value=CvBridge())
 @nrp.MapVariable("points", initial_value=[], scope=nrp.GLOBAL)
-@nrp.MapVariable("camera_model", initial_value=PinholeCameraModel())
+@nrp.MapVariable("camera_model", initial_value=StereoCameraModel())
 @nrp.MapVariable("camera_info_left", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("camera_info_right", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("disparity_image", initial_value=None, scope=nrp.GLOBAL)
+@nrp.MapVariable("transform_proxy", initial_value=rospy.ServiceProxy("/transform", Transform))
 @nrp.MapVariable("last_time", initial_value = None)
 @nrp.MapVariable("elapsed", initial_value = 0)
-@nrp.MapVariable("pan", initial_value = 0, scope=nrp.GLOBAL)
-@nrp.MapVariable("tilt", initial_value = 0, scope=nrp.GLOBAL)
 @nrp.MapRobotSubscriber("image", Topic("/hollie/camera/left/image_raw", Image))
-def image_to_saliency(t, image, bridge, saliency, saliency_pub, saliency_image_pub, points, camera_model, camera_info_left, last_time, elapsed, pan, tilt):
+def image_to_saliency(t, image, bridge, saliency, saliency_pub, saliency_image_pub, points, camera_model, camera_info_left, camera_info_right, disparity_image, transform_proxy, last_time, elapsed):
     if t < 1.0:
         return
 
-    if image.value is None or camera_info_left.value is None:
+    if image.value is None or camera_info_left.value is None or camera_info_right.value is None or disparity_image.value is None:
         return
 
     if last_time.value is None:
@@ -47,12 +48,27 @@ def image_to_saliency(t, image, bridge, saliency, saliency_pub, saliency_image_p
     saliency_map = saliency.value.compute_saliency_map(image)
 
     # apply curiosity
-    camera_model.value.fromCameraInfo(camera_info_left.value)
+    import rospy
+    import geometry_msgs
+    import tf2_geometry_msgs
+    camera_model.value.fromCameraInfo(camera_info_left.value, camera_info_right.value)
+    disparity_image = bridge.value.imgmsg_to_cv2(disparity_image.value.image)
     for point in points.value:
         # call service
-        pixel = camera_model.value.project3dToPixel((point.x - pan.value.data, point.y - tilt.value.data, point.z))
-        x = int(pixel[0] * (len(saliency_map[0])/float(camera_info_left.value.width)))
-        y = int(pixel[1] * (len(saliency_map)/float(camera_info_left.value.height)))
+        point_new = geometry_msgs.msg.PointStamped()
+        point_new.header = point.header
+        point_new.point = point.point
+        transformed = transform_proxy.value(point_new).res
+        transformed_new = tf2_geometry_msgs.PointStamped()
+        transformed_new.header = transformed.header
+        transformed_new.point = transformed.point
+        transformed = transformed_new
+        point_torso = (-transformed.point.y, -transformed.point.z, transformed.point.x)
+        pixel = camera_model.value.project3dToPixel(point_torso)
+        x = int(pixel[0][0] * (len(saliency_map[0])/float(camera_info_left.value.width)))
+        y = int(pixel[0][1] * (len(saliency_map)/float(camera_info_left.value.height)))
+        disparity = camera_model.value.getDisparity(point_torso[2])
+        x = x + disparity
         if x >= 0 and x < len(saliency_map[0]) and y >=0 and y < len(saliency_map):
             from skimage.draw import circle
             rr, cc = circle(y, x, 25, (len(saliency_map), len(saliency_map[0])))
